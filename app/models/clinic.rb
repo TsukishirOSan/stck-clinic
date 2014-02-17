0a33f4c9b1d36627eb6808440c1763fe451adec0
Jonathan E. Magen <yonkeltron@gmail.com>
2014-02-17 18:19:06 -0500

    basic info specs

--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -1,3 +1,13 @@
 class Clinic < ActiveRecord::Base
   validates :name, presence: true
+  # basic info
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :street_address, presence: true
+  validates :zip, presence: true
+  # contact info
+  validates :contact_email, presence: true
+  validates :contact_name, presence: true
+  validates :contact_phone, presence: true
+  validates :contact_title, presence: true
 end
