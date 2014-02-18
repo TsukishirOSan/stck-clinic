bc29f1dbf8e1651514de7b8d19102b90438efb39
Jonathan E. Magen <yonkeltron@gmail.com>
2014-02-18 14:02:00 -0500

    whitespace

--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -1,6 +1,6 @@
 class Clinic < ActiveRecord::Base
-  validates :name, presence: true
   # basic info
+  validates :name, presence: true
   validates :city, presence: true
   validates :state, presence: true
   validates :street_address, presence: true
