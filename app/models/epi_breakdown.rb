5677b82285807737afdb4caad7f9b146093045dc
Jonathan E. Magen <yonkeltron@gmail.com>
2014-02-20 12:23:41 -0500

    validation specs for epi_breakdown

--- app/models/epi_breakdown.rb
+++ app/models/epi_breakdown.rb
@@ -1,3 +1,10 @@
 class EpiBreakdown < ActiveRecord::Base
   belongs_to :clinic
+
+  [:test_ct,    :diag_ct,
+   :test_gc,    :diag_gc,
+   :test_trich, :diag_trich,
+   :test_hiv,   :diag_hiv].each do |column_name|
+    validates column_name, numericality: true
+  end
 end
--- spec/models/epi_breakdown_spec.rb
+++ spec/models/epi_breakdown_spec.rb
@@ -2,4 +2,18 @@ require 'spec_helper'
 
 describe EpiBreakdown do
   it { should belong_to(:clinic) }
+
+  it { should validate_numericality_of(:test_ct) }
+  it { should validate_numericality_of(:diag_ct) }
+  it { should validate_numericality_of(:test_gc) }
+  it { should validate_numericality_of(:diag_gc) }
+  it { should validate_numericality_of(:test_trich) }
+  it { should validate_numericality_of(:diag_trich) }
+  it { should validate_numericality_of(:test_hiv) }
+  it { should validate_numericality_of(:diag_hiv) }
+
+  context 'given valid attributes' do
+    subject { FactoryGirl.build(:epi_breakdown) }
+    it { should be_valid }
+  end
 end
