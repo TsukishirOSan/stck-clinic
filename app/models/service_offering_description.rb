515e6e01abe0fdcb536b3d1b506507ba184a05cf
Jonathan E. Magen <yonkeltron@gmail.com>
2014-02-24 11:40:34 -0500

    annotate

--- app/models/service_offering_description.rb
+++ app/models/service_offering_description.rb
@@ -2,18 +2,25 @@
 #
 # Table name: service_offering_descriptions
 #
-#  id                 :integer          not null, primary key
-#  name               :string(255)
-#  clinic_id          :integer
-#  college_health     :boolean
-#  community_health   :boolean
-#  family_planning    :boolean
-#  planned_parenthood :boolean
-#  private_practice   :boolean
-#  std                :boolean
-#  other              :boolean
-#  created_at         :datetime
-#  updated_at         :datetime
+#  id                                 :integer          not null, primary key
+#  name                               :string(255)
+#  clinic_id                          :integer
+#  college_health                     :boolean
+#  community_health                   :boolean
+#  family_planning                    :boolean
+#  planned_parenthood                 :boolean
+#  private_practice                   :boolean
+#  std                                :boolean
+#  other                              :boolean
+#  created_at                         :datetime
+#  updated_at                         :datetime
+#  charge_ct                          :string(255)
+#  notify_test_ready                  :string(255)
+#  deliver_results_online             :boolean
+#  deliver_results_in_person          :boolean
+#  deliver_results_on_phone_human     :boolean
+#  deliver_results_on_phone_automated :boolean
+#  deliver_results_other              :boolean
 #
 
 class ServiceOfferingDescription < ActiveRecord::Base
