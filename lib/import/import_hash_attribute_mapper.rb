90f0dfd498ad54438d6f5e3616c990253f720638
Jonathan E. Magen <yonkeltron@gmail.com>
2014-03-03 18:26:43 -0500

    style

--- lib/import/import_hash_attribute_mapper.rb
+++ lib/import/import_hash_attribute_mapper.rb
@@ -6,37 +6,37 @@ class ImportHashAttributeMapper < Struct.new(:import_hash)
   # Data we can map directly, which require no processing
   DIRECT_ATTRIBUTE_MAP_TABLE = {
     # Clinic info
-    "Clinic name" => :clinic_name,
-    "Address line 1" => :clinic_street_address,
-    "Address line 2" => :clinic_street_address_continued,
-    "City" => :clinic_city,
-    "State" => :clinic_state,
-    "Zip code" => :clinic_zip,
+    'Clinic name'                                              => :clinic_name,
+    'Address line 1'                                           => :clinic_street_address,
+    'Address line 2'                                           => :clinic_street_address_continued,
+    'City'                                                     => :clinic_city,
+    'State'                                                    => :clinic_state,
+    'Zip code'                                                 => :clinic_zip,
     # Contact info
-    "Main contact name" => :contact_name,
-    "Phone number" => :contact_phone,
-    "Main contact email" => :contact_email,
-    "Title" => :contact_title,
+    'Main contact name'                                        => :contact_name,
+    'Phone number'                                             => :contact_phone,
+    'Main contact email'                                       => :contact_email,
+    'Title'                                                    => :contact_title,
     # charges info
-    "Do you notify clients when their test results are ready?" => :notify_test_ready,
-    "How much do you charge clients for a chlamydia test?" => :charge_ct
+    'Do you notify clients when their test results are ready?' => :notify_test_ready,
+    'How much do you charge clients for a chlamydia test?'     => :charge_ct
   }
 
   # Data we can map after processing with numeric filter
   NUMERIC_ATTRIBUTE_MAP_TABLE = {
-    "What percent of your clients are women?" => :population_women,
-    "What percent of your clients are men who have sex with men (MSM)?" => :population_msm,
-    "What percent of your clients are under 26?" => :population_under_26,
-    "What percent of your clients are black/African American?" => :population_black,
-    "What percent of your clients are hispanic/Latino(a)?" => :population_hispanic,
-    "How many people did you test last year for chlamydia?" => :test_ct,
-    "How many did you diagnose with chlamydia?" => :diag_ct,
-    "How many did you test for gonorrhea?" => :test_gc,
-    "How many did you diagnose with gonorrhea?" => :diag_gc,
-    "How many did you test for trichomoniasis?" => :test_trich,
-    "How may did you diagnose with trichomoniasis?" => :diag_trich,
-    "How many did you test for HIV?" => :test_hiv,
-    "How many did you diagnose with HIV?" => :diag_hiv,
+    'What percent of your clients are women?'                           => :population_women,
+    'What percent of your clients are men who have sex with men (MSM)?' => :population_msm,
+    'What percent of your clients are under 26?'                        => :population_under_26,
+    'What percent of your clients are black/African American?'          => :population_black,
+    'What percent of your clients are hispanic/Latino(a)?'              => :population_hispanic,
+    'How many people did you test last year for chlamydia?'             => :test_ct,
+    'How many did you diagnose with chlamydia?'                         => :diag_ct,
+    'How many did you test for gonorrhea?'                              => :test_gc,
+    'How many did you diagnose with gonorrhea?'                         => :diag_gc,
+    'How many did you test for trichomoniasis?'                         => :test_trich,
+    'How may did you diagnose with trichomoniasis?'                     => :diag_trich,
+    'How many did you test for HIV?'                                    => :test_hiv,
+    'How many did you diagnose with HIV?'                               => :diag_hiv
   }
 
   Contract nil => Hash
@@ -67,7 +67,10 @@ class ImportHashAttributeMapper < Struct.new(:import_hash)
   def mapped_numeric_attribute_hash
     pairs = NUMERIC_ATTRIBUTE_MAP_TABLE.map do |src, dest|
       Rails.logger.info("Mapping numeric '#{src}' -> '#{dest}'")
-      [ dest, value_to_float(import_hash[src]) ]
+      [
+        dest,
+        value_to_float(import_hash[src])
+      ]
     end
 
     Hash[pairs]
