7bfabf0691dd697e3eae0546cfca0829a730573b
Jonathan E. Magen <yonkeltron@gmail.com>
2014-03-10 14:16:24 -0400

    disable registrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 StckClinic::Application.routes.draw do
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
-  devise_for :users
+  devise_for :users, skip: :registrations
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
