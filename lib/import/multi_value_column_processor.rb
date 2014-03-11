2ee5eab90cc9c8612a88032ddef49d22c106384b
Jonathan E. Magen <yonkeltron@gmail.com>
2014-03-11 10:48:01 -0400

    we can now import columns with multiple values (for checkboxen)

---
+++ lib/import/multi_value_column_processor.rb
@@ -0,0 +1,33 @@
+# Processes multi value columns containing comma-sep data
+class MultiValueColumnProcessor < Struct.new(:raw_column_value, :transformation_map)
+  include Contracts
+
+  Contract nil => Hash
+  # maps import column values to their model attribute equivalents
+  # @return [Hash] the mapped attribute hash
+  def values_hash
+    column_values = preprocess_column(raw_column_value)
+
+    # checking to make sure we know how to map everything
+    column_values.each do |column_value|
+      fail "Don't know how to map value #{column_value}!" unless transformation_map.has_key?(column_value)
+    end
+
+    # actually do the mapping
+    transformation_map.map do |value, destination_attribute|
+      {
+        destination_attribute => column_values.include?(value)
+      }
+    end.reduce(&:merge)
+  end
+
+  Contract String => ArrayOf[String]
+  # splits column on comma character and cleans up values
+  # @param [String] the raw column
+  # @return [Array<String>] the array of cleaned up string values
+  def preprocess_column(raw_column)
+    raw_column.split(',')
+      .map(&:downcase)
+      .map(&:strip)
+  end
+end
---
+++ spec/lib/import/multi_value_column_processor_spec.rb
@@ -0,0 +1,64 @@
+require 'spec_helper'
+
+describe MultiValueColumnProcessor do
+  let(:test_column_data) { 'Panda, Bamboo, Curry (noodle)' }
+
+  let(:test_transformation_map) do
+    {
+      'panda'          => :panda_column,
+      'bamboo'         => :bamboo_column,
+      'curry (noodle)' => :curry_column,
+      'tree'           => :tree_column
+    }
+  end
+
+  let(:multi_value_column_processor) { MultiValueColumnProcessor.new(test_column_data, test_transformation_map) }
+
+  describe '#preprocess_column' do
+    let(:processed_columns) { multi_value_column_processor.preprocess_column(test_column_data) }
+    context 'given three values in the column' do
+      context 'length' do
+        it { expect(processed_columns.length).to be(3) }
+      end
+    end
+
+    context 'values' do
+      context 'with mixed-case values' do
+        it do
+          processed_columns.each {|value| expect(value).not_to match(/[A-Z]/) }
+        end
+      end
+
+      context 'with whitespace' do
+        it do
+          processed_columns.each {|value| expect(value).not_to match(/(^\s|\s$)/) }
+        end
+      end
+    end
+  end
+
+  describe '#values_hash' do
+    let(:values_hash) { multi_value_column_processor.values_hash }
+
+    context 'given a found mapping' do
+      [:panda_column, :bamboo_column, :curry_column].each do |column|
+        context column do
+          it { expect(values_hash[column]).to be(true) }
+        end
+      end
+    end
+
+    context 'given no provided value' do
+      it { expect(values_hash[:tree_column]).to be(false) }
+    end
+
+    context 'given no found mapping' do
+      it 'pukes' do
+        expect do
+          missing_column_data = "Panda, Missing (should fail terribly)"
+          MultiValueColumnProcessor.new(missing_column_data, test_transformation_map).values_hash
+        end.to raise_error(RuntimeError, "Don't know how to map value missing (should fail terribly)!")
+      end
+    end
+  end
+end
