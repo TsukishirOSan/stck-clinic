3812493f7aa01523ec70bc1f642eec86919035cb
Jonathan E. Magen <yonkeltron@gmail.com>
2014-03-13 15:39:13 -0400

    proper describe block

--- spec/mailers/clinic_mailer_spec.rb
+++ spec/mailers/clinic_mailer_spec.rb
@@ -2,23 +2,27 @@ require "spec_helper"
 
 describe ClinicMailer do
   let(:clinic) { FactoryGirl.create(:clinic) }
-  let(:mail) { ClinicMailer.new_clinic_email(clinic) }
 
-  describe 'message body' do
-    subject { mail.body }
+  describe '.new_clinic_email' do
+    let(:mail) { ClinicMailer.new_clinic_email(clinic) }
 
-    it { should match("Lucky number") }
-    it { should match(Clinic.model_name.human) }
-    it { should match(clinic.name) }
-  end
+    describe 'message body' do
+      subject { mail.body }
 
-  describe 'meta' do
-    context 'recipient' do
-      it { expect(mail.to).to eq(['stckclinic@sexualhealthinnovations.org']) }
+      it { should match("Lucky number") }
+      it { should match(Clinic.model_name.human) }
+      it { should match(clinic.name) }
     end
 
-    context 'sender' do
-      it { expect(mail.from).to eq(['noreply+StckClinicMonkey@sexualhealthinnovations.org']) }
+    describe 'meta' do
+      context 'recipient' do
+        it { expect(mail.to).to eq(['stckclinic@sexualhealthinnovations.org']) }
+      end
+
+      context 'sender' do
+        it { expect(mail.from).to eq(['noreply+StckClinicMonkey@sexualhealthinnovations.org']) }
+      end
     end
   end
 end
+
